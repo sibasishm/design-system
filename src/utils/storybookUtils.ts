110fb1c5714bd81745b90bc28a4339de0a59c1f3
Sibasish Mohanty <sibasish.mohanty@goodera.com>
2021-03-22 20:39:04 +0530

    types added

--- src/utils/storybookUtils.ts
+++ src/utils/storybookUtils.ts
@@ -1,23 +1,24 @@
-type ControlType = {
+interface ControlType<T> {
 	control: {
 		type: 'inline-radio' | 'boolean' | 'text';
+		options?: T;
 	};
-};
+}
 
-export const toEnum = <T>(options: T[]) => ({
+export const toEnum = <T>(options: T[]): ControlType<T[]> => ({
 	control: {
 		type: 'inline-radio',
 		options,
 	},
 });
 
-export const toBoolean = () => ({
+export const toBoolean = (): ControlType<null> => ({
 	control: {
 		type: 'boolean',
 	},
 });
 
-export const toStr = () => ({
+export const toStr = (): ControlType<null> => ({
 	control: {
 		type: 'text',
 	},
