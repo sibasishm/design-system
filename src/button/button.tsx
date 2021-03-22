de19231628661a0e67cef4938d63c5bacd0c8d09
Sibasish Mohanty <sibasish.mohanty@goodera.com>
2021-03-22 18:23:43 +0530

    added button component

---
+++ src/button/button.tsx
@@ -0,0 +1,41 @@
+import React, {
+	ButtonHTMLAttributes,
+	DetailedHTMLProps,
+	ReactNode,
+} from 'react';
+
+import { variants, sizes, colors, generateClassName } from './variations';
+
+export type ButtonProps = DetailedHTMLProps<
+	ButtonHTMLAttributes<HTMLButtonElement>,
+	HTMLButtonElement
+> & {
+	size?: keyof typeof sizes;
+	color?: typeof colors[number];
+	variant?: typeof variants[number];
+	/**
+	 * If added, the button will show an icon before the button's label.
+	 */
+	icon?: ReactNode;
+};
+
+export const Button = ({
+	children,
+	size = 'big',
+	color = 'primary',
+	variant = 'solid',
+	icon,
+	...props
+}: ButtonProps): JSX.Element => {
+	return (
+		<button
+			className={`flex items-center justify-center font-bold ${
+				sizes[size]
+			} ${generateClassName(variant, color)} disabled:text-dark-offset`}
+			{...props}
+		>
+			{icon ? <span className='mr-2'>{icon}</span> : null}
+			{children}
+		</button>
+	);
+};
